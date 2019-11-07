596f5040bf59561192465fe0618aa575f0774406
IGOR DO NASCIMENTO ALVES <unifigalves@fei.edu.br>
2019-11-06 22:12:38 -0300

    commit zik

--- GeradorDocumentosServer/src/geradordocumentosserver/GeradorCliente.java
+++ GeradorDocumentosServer/src/geradordocumentosserver/GeradorCliente.java
@@ -5,24 +5,40 @@
  */
 package geradordocumentosserver;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-/**
- *
- * @author unifrasilva
- */
 public class GeradorCliente {
     
-    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException, InterruptedException{
-        
+    public static void main(String[] args) throws NotBoundException, InterruptedException, IOException{
+    	/*
+    	FileReader arq = new FileReader("C:/Users/igor_/Documents/GitHub/RMIServer/GeradorDocumentosServer/src/geradordocumentosserver/teste.txt");
+        BufferedReader lerArq = new BufferedReader(arq);
+        List<String> lista = new ArrayList<String>();
+        String linha = lerArq.readLine();
+        while (linha != null) {
+        	lista.add(linha);
+          System.out.printf("%s\n", linha);
+          linha = lerArq.readLine(); // lê da segunda até a última linha
+          
+        }
+        arq.close();
+    	*/
         GeradorInterface remoto = null;
-        Registry reg=LocateRegistry.getRegistry("169.254.40.49",1429);
+        Registry reg=LocateRegistry.getRegistry("169.254.37.149",1429);
+        String cavalo;
         remoto = (GeradorInterface) reg.lookup("japones");
+        //cavalo = remoto.executa(lista,1);
         int option = 3;
         Scanner s = new Scanner(System.in);      
         String doc = null;
@@ -33,16 +49,21 @@ public class GeradorCliente {
             if(option == 1){
                 System.out.println("\nFavor digitar 9 digitos para CPF:");
                 doc = s.next();
-                if(doc.length() == 9 )
-                    remoto.executa(doc,option);
+                
+                if(doc.length() == 9 ) {
+                	cavalo = remoto.executa(doc,option);
+                	System.out.println("CNPJ gerado" + cavalo);
+                }
                 else
                     System.out.println("\nNumero de digitos invalido");    
             }
             if(option == 2){
-                System.out.println("\nFavor digitar 11 digitos para CNPJ:");
+                System.out.println("\nFavor digitar 12 digitos para CNPJ:");
                 doc = s.next();
-                if(doc.length() == 9 )
-                    remoto.executa(doc,option);
+                if(doc.length() == 12 ) {
+                	cavalo = remoto.executa(doc,option);
+                	System.out.println("CPF gerado" + cavalo);
+                }
                 else
                     System.out.println("\nNumero de digitos invalido");    
             }
